1b19791b0224da38bb49c4dbf99eb6e0665052bf
James_Worcester <Worcester_James@bah.com>
2016-07-13 11:48:50 -0400

    Storing padded flag implementation in a separate branch in case we need it

--- dataactvalidator/filestreaming/fieldCleaner.py
+++ dataactvalidator/filestreaming/fieldCleaner.py
@@ -125,8 +125,10 @@ class FieldCleaner(StringCleaner):
                     # Replace empty strings with null
                     value = None
 
-                row[key] = cls.padField(key,value,fileType,validationInterface)
-
+                (row[key],wasPadded) = cls.padField(key,value,fileType,validationInterface)
+                if wasPadded:
+                    # Mark padded flag true
+                    row["".join([key,"_padded"])] = True
         return row
 
     @staticmethod
@@ -140,7 +142,7 @@ class FieldCleaner(StringCleaner):
             interfaces: InterfaceHolder object
 
         Returns:
-            Padded value
+            Padded value and a boolean indicating whether padding occurred
         """
         # Check padded flag for this field and file
         if value is not None and validationInterface.isPadded(field,fileType):
@@ -148,9 +150,9 @@ class FieldCleaner(StringCleaner):
             padLength = validationInterface.getColumnLength(field, fileType)
             if len(value) < padLength:
                 # Pad to specified length with leading zeros
-                return value.zfill(padLength)
-        # Padding did not occur, return original value
-        return value
+                return value.zfill(padLength), True
+        # Padding did not occur, return original value and False
+        return value, False
 
 if __name__ == '__main__':
     FieldCleaner.cleanFile("../config/appropFieldsRaw.csv","../config/appropFields.csv")
